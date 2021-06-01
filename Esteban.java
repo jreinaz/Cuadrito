d7574cf484d294a37343039a26958cd121fca129
CarlosKC26 <48418226+CarlosKC26@users.noreply.github.com>
2021-05-31 23:15:27 -0500

    Add files via upload

--- Esteban.java
+++ Esteban.java
@@ -1,82 +1,101 @@
 package uniltiranyu.examples.games.squares;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-import speco.array.Array;
+
+
 import uniltiranyu.Action;
 import uniltiranyu.AgentProgram;
 import uniltiranyu.Percept;
 
 public class Esteban implements AgentProgram {
-	
+
 	protected String color;
-    protected int[][] board = null;
-    protected int[][] values = null;
-    protected int size = 0;
-    protected int[] lastBox = new int[2];
-    protected int phase = 0;
-    protected int cuenta = 0;
+	protected int[][] board = null;
+	protected int[][] values = null;
+	protected int size = 0;
+	protected int[] lastBox = new int[2];
+	protected int phase = 0;
+	protected int cuenta = 0;
 	protected boolean recheck = true;
-	protected Array<String> moves = new Array<>();
+	protected ArrayList<String> moves = new ArrayList<>();
 	protected int[] lastPosition = new int[2];
 	protected boolean played = false;
-	
-    public Esteban(String color) {
+	protected ArrayList<String> historial = new ArrayList<>();
+	protected int julio = 0;
+
+	public Esteban(String color) {
 		this.color = color;
 		this.lastBox[0] = 0;
 		this.lastBox[1] = 1;
 	}
-    
+
 	@Override
 	public Action compute(Percept p) {
-		
-		if(size == 0){ // Gets the size of the board
-        	size = Integer.parseInt((String)p.get(Squares.SIZE));
-        	int x = size > 13? (int) (size * 1.2) : 15;
-        	detenerse(x);
-        	//@ToDo probar size*1.5
+
+		if (size == 0) { // Gets the size of the board
+			size = Integer.parseInt((String) p.get(Squares.SIZE));
+			julio = size > 13 ? (int) (size * 1.2) : 15;
+			phase = size > 4 ? 0:1;
+			detenerse(julio);
+			// @ToDo probar size*1.5
 		}
-        if(board == null) createBoard();
-        
-        // Determines if it is the agents turn
-        if( p.get(Squares.TURN).equals(color) ){
+		if (board == null)
+			createBoard();
+
+		// Determines if it is the agents turn
+		if (p.get(Squares.TURN).equals(color)) {
 			// Esteban turn
-        	played = false;
-        	int[] puntos = null;
-        	while(!played) {
-	        	switch(phase) {
-	        		case 0:
-						int[] box = pahse0(p);
-						try {
-							String move = moves.get((int) (Math.random() * moves.size()));
-							moves.clear();
-	                        if (phase != 1 && played) return new Action(box[0] + ":" + box[1] + ":" + move);
-	        			} catch (Exception e) {}
-	        			break;
-	        		case 1:
-	        			// min-max	        			
-						//puntos=actualizarTablero(p);
-						//
-						phase = 2;
-	        			break; 	
-	        		case 2:
-	        			actualizarTablero(p);        			
-	        			Action move = phase2();
+			played = false;
+			int[] puntos = null;
+			while (!played) {
+				switch (phase) {
+				case 0:
+					int[] box = pahse0(p);
+					try {
+						String move = moves.get((int) (Math.random() * moves.size()));
 						moves.clear();
-						try {
-							return move;
-						} catch (Exception e) {}
-	        			break;
-	        		default:
-	        			break;
-	        	}
-        	}
-        	System.out.println("XD");
-        	return new Action(lastBox[0] + ":" + lastBox[1] + ":" + Squares.PASS);
-        } else {
-        	// Opponents turn
-        }
-        return new Action(lastBox[0] + ":" + lastBox[1] + ":" + Squares.PASS);
+						if (phase != 1 && played)
+							return new Action(box[0] + ":" + box[1] + ":" + move);
+					} catch (Exception e) {
+					}
+					break;
+				case 1:
+					// min-max
+					puntos=actualizarTablero(p);
+					//
+					values = Arrays.stream(board).map(int[]::clone).toArray(int[][]::new);
+					String jugada = minMax("0:0:"+Squares.PASS,true,10,puntos);
+					String[] s=jugada.split(":");
+					jugada = s[0]+":"+s[1]+":"+s[2];
+					try {
+						return new Action(jugada);
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+					
+					//phase = 2;
+					break;
+				case 2:
+					actualizarTablero(p);
+					Action move = phase2();
+					moves.clear();
+					try {
+						return move;
+					} catch (Exception e) {
+					}
+					break;
+				default:
+					break;
+				}
+			}
+			System.out.println("XD");
+			return new Action(lastBox[0] + ":" + lastBox[1] + ":" + Squares.PASS);
+		} else {
+			// Opponents turn
+		}
+		return new Action(lastBox[0] + ":" + lastBox[1] + ":" + Squares.PASS);
 	}
 
 	@Override
@@ -96,28 +115,32 @@ public class Esteban implements AgentProgram {
 			values[i][j] |= Board.RIGHT;
 			values[i][j + 1] |= Board.LEFT;
 			check(i, j);
-			if (recheck) check(i, j + 1);
+			if (recheck)
+				check(i, j + 1);
 			resetValues(i, j, Squares.RIGHT);
 		}
 		if ((values[i][j] & Board.BOTTOM) != Board.BOTTOM) {
 			values[i][j] |= Board.BOTTOM;
 			values[i + 1][j] |= Board.TOP;
 			check(i, j);
-			if (recheck) check(i + 1, j);
+			if (recheck)
+				check(i + 1, j);
 			resetValues(i, j, Squares.BOTTOM);
 		}
 		if ((values[i][j] & Board.LEFT) != Board.LEFT) {
 			values[i][j] |= Board.LEFT;
 			values[i][j - 1] |= Board.RIGHT;
 			check(i, j);
-			if (recheck) check(i, j - 1);
+			if (recheck)
+				check(i, j - 1);
 			resetValues(i, j, Squares.LEFT);
 		}
 		if ((values[i][j] & Board.TOP) != Board.TOP) {
 			values[i][j] |= Board.TOP;
 			values[i - 1][j] |= Board.BOTTOM;
 			check(i, j);
-			if (recheck) check(i - 1, j);
+			if (recheck)
+				check(i - 1, j);
 			resetValues(i, j, Squares.TOP);
 		}
 	}
@@ -133,11 +156,11 @@ public class Esteban implements AgentProgram {
 		}
 		String min = Squares.PASS;
 		int minMov = Integer.MAX_VALUE;
-		for (String s:moves) {
+		for (String s : moves) {
 			String[] aux = s.split(":");
-			if (Integer.parseInt(aux[3])<minMov) {
-				minMov=Integer.parseInt(aux[3]);
-				min = aux[0]+":"+aux[1]+":"+aux[2];
+			if (Integer.parseInt(aux[3]) < minMov) {
+				minMov = Integer.parseInt(aux[3]);
+				min = aux[0] + ":" + aux[1] + ":" + aux[2];
 			}
 		}
 		if (!min.equals(Squares.PASS))
@@ -149,16 +172,16 @@ public class Esteban implements AgentProgram {
 		int[] box = new int[2];
 		out: for (int i = lastBox[0]; i < size; i++) {
 			for (int j = lastBox[1]; j < size; j++) {
-				//turnos * 2 + 70 >= ((size * size * 4) - (4 * size) - ((size * size * 4) - (4 * size)) / 2)
-				/*if (i >= lastPosition[0] && j >= lastPosition[1]) {
-					phase = 1;
-					break out;
-				}*/
-				if (i == size-1 && j == size-1) {
-					phase = 1;
-					break out;
-				}
+				// turnos * 2 + 70 >= ((size * size * 4) - (4 * size) - ((size * size * 4) - (4
+				// * size)) / 2)
 				
+				if (i >= lastPosition[0] && j >= lastPosition[1]) { phase = 1; break out; }
+				 
+//				if (i == size - 1 && j == size - 1) {
+//					phase = 1;
+//					break out;
+//				}
+
 				if (lines(i, j, p) < 2) {
 					if (avalible(i, j, Squares.RIGHT, true, p)) {
 						if (lines(i, j + 1, p) < 2)
@@ -189,124 +212,275 @@ public class Esteban implements AgentProgram {
 		return ((String) p.get(y + ":" + x + ":" + line)).equals(Squares.FALSE) && y + 1 != size;
 	}
 
-	protected int lines(int i, int j, Percept p){
-		int c = ((String) p.get(i + ":" + j + ":" + Squares.LEFT)).equals(Squares.TRUE)? 1 : 0;
-			c += ((String) p.get(i + ":" + j +":" + Squares.TOP)).equals(Squares.TRUE)? 1 : 0;
-			c += ((String) p.get(i + ":" + j +":" + Squares.RIGHT)).equals(Squares.TRUE)? 1 : 0;
-			c += ((String) p.get(i + ":" + j +":" + Squares.BOTTOM)).equals(Squares.TRUE)? 1 : 0;
+	protected int lines(int i, int j, Percept p) {
+		int c = ((String) p.get(i + ":" + j + ":" + Squares.LEFT)).equals(Squares.TRUE) ? 1 : 0;
+		c += ((String) p.get(i + ":" + j + ":" + Squares.TOP)).equals(Squares.TRUE) ? 1 : 0;
+		c += ((String) p.get(i + ":" + j + ":" + Squares.RIGHT)).equals(Squares.TRUE) ? 1 : 0;
+		c += ((String) p.get(i + ":" + j + ":" + Squares.BOTTOM)).equals(Squares.TRUE) ? 1 : 0;
 		return c;
 	}
 
-	private int[]  actualizarTablero(Percept p) {
-		int[] puntos = new int [2];
-    	String s;
-		for(int i = 0;i < size;i++) {
-			for(int j = 0;j < size;j++) {
+	private int[] actualizarTablero(Percept p) {
+		int[] puntos = new int[2];
+		String s;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				if (((String) p.get(i + ":" + j + ":" + Squares.LEFT)).equals(Squares.TRUE))
 					board[i][j] |= Board.LEFT;
 				if (((String) p.get(i + ":" + j + ":" + Squares.TOP)).equals(Squares.TRUE))
 					board[i][j] |= Board.TOP;
-    			if(((String)p.get(i+":"+j+":"+Squares.BOTTOM)).equals(Squares.TRUE))
-    				board[i][j] |= Board.BOTTOM;
-    			if(((String)p.get(i+":"+j+":"+Squares.RIGHT)).equals(Squares.TRUE))
-    				board[i][j] |= Board.RIGHT;
-    			s = (String) p.get(i + ":" + j);
-    			if (s.equals(color)) {
-    				board[i][j] |= Board.WHITE;
-    				puntos[0]++;
-    			}else if (!s.equals(Squares.SPACE)) {
-    				board[i][j] |= Board.BLACK;
-    				puntos[1]++;
-    			}
-    		}
-    	}
+				if (((String) p.get(i + ":" + j + ":" + Squares.BOTTOM)).equals(Squares.TRUE))
+					board[i][j] |= Board.BOTTOM;
+				if (((String) p.get(i + ":" + j + ":" + Squares.RIGHT)).equals(Squares.TRUE))
+					board[i][j] |= Board.RIGHT;
+				s = (String) p.get(i + ":" + j);
+				if (s.equals(color)) {
+					board[i][j] |= Board.WHITE;
+					puntos[0]++;
+				} else if (!s.equals(Squares.SPACE)) {
+					board[i][j] |= Board.BLACK;
+					puntos[1]++;
+				}
+			}
+		}
 		return puntos;
-    }
+	}
 
 	protected void createBoard() {
 		board = new int[size][size];
-		for( int i = 0;i < size;i++ ){
+		for (int i = 0; i < size; i++) {
 			board[i][0] = Board.LEFT;
-			board[i][size-1] = Board.RIGHT;
+			board[i][size - 1] = Board.RIGHT;
 		}
 		for (int i = 0; i < size; i++) {
 			board[0][i] |= Board.TOP;
 			board[size - 1][i] |= Board.BOTTOM;
 		}
-//            for(int i = 0;i < size;i++) {
-//            	for(int j = 0;j < size;j++) {
-//            		System.out.print(board[i][j] + " ");
-//            	}
-//            	System.out.println();
-//            }
+		// for(int i = 0;i < size;i++) {
+		// for(int j = 0;j < size;j++) {
+		// System.out.print(board[i][j] + " ");
+		// }
+		// System.out.println();
+		// }
 	}
 
 	protected int lines(int i, int j) {
-		int c=(values[i][j] & Board.LEFT)==Board.LEFT?1:0;
-			c+=(values[i][j] & Board.TOP)==Board.TOP?1:0;
-			c+=(values[i][j] & Board.RIGHT)==Board.RIGHT?1:0;
-			c+=(values[i][j] & Board.BOTTOM)==Board.BOTTOM?1:0;
-	    return c;
+		int c = (values[i][j] & Board.LEFT) == Board.LEFT ? 1 : 0;
+		c += (values[i][j] & Board.TOP) == Board.TOP ? 1 : 0;
+		c += (values[i][j] & Board.RIGHT) == Board.RIGHT ? 1 : 0;
+		c += (values[i][j] & Board.BOTTOM) == Board.BOTTOM ? 1 : 0;
+		return c;
+	}
+
+	protected boolean closable(int i, int j) {
+		return lines(i, j) == 3;
+	}
+
+	protected boolean closed(int i, int j) {
+		return lines(i, j) == 4;
+	}
+
+	protected void check(int i, int j) {
+		if (closed(i, j)) {
+			cuenta++;
+			recheck = false;
+		} else {
+			if (closable(i, j)) {
+				if ((values[i][j] & Board.LEFT) == 0) {
+					values[i][j] |= Board.LEFT;
+					values[i][j - 1] |= Board.RIGHT;
+					cuenta++;
+					check(i, j - 1);
+				}
+
+				if ((values[i][j] & Board.TOP) == 0) {
+					values[i][j] |= Board.TOP;
+					values[i - 1][j] |= Board.BOTTOM;
+					cuenta++;
+					check(i - 1, j);
+				}
+
+				if ((values[i][j] & Board.RIGHT) == 0) {
+					values[i][j] |= Board.RIGHT;
+					values[i][j + 1] |= Board.LEFT;
+					cuenta++;
+					check(i, j + 1);
+				}
+
+				if ((values[i][j] & Board.BOTTOM) == 0) {
+					values[i][j] |= Board.BOTTOM;
+					values[i + 1][j] |= Board.TOP;
+					cuenta++;
+					check(i + 1, j);
+				}
+			}
+		}
+	}
+
+	protected void detenerse(int a) {
+		stop: for (int i = size - 1; i >= 0; i--) {
+			for (int j = size - 1; j >= 0; j--) {
+				cuenta++;
+
+				if (cuenta == a) {
+					lastPosition[0] = i;
+					lastPosition[1] = j;
+					break stop;
+				}
+			}
+		}
+	}
+
+	protected void sacarJugadas(int maximasJugadas) {
+		int x, y;
+		for (int i = 0; i < maximasJugadas; i++) {
+			while (true) {
+				y = (int) (lastPosition[0] + (size - lastPosition[0]) * Math.random());
+				x = (int) (size * Math.random());
+				if (x == 8 || y == 8)
+					continue;
+				if ((values[y][x] & Board.LEFT) == 0 && !moves.contains(y + ":" + x + ":" + Squares.LEFT)) {
+					moves.add(y + ":" + x + ":" + Squares.LEFT);
+					break;
+				}
+				if ((values[y][x] & Board.TOP) == 0 && !moves.contains(y + ":" + x + ":" + Squares.TOP)) {
+					moves.add(y + ":" + x + ":" + Squares.TOP);
+					break;
+				}
+				if ((values[y][x] & Board.BOTTOM) == 0 && !moves.contains(y + ":" + x + ":" + Squares.BOTTOM)) {
+					moves.add(y + ":" + x + ":" + Squares.BOTTOM);
+					break;
+				}
+				if ((values[y][x] & Board.RIGHT) == 0  && !moves.contains(y + ":" + x + ":" + Squares.RIGHT)) {
+					moves.add(y + ":" + x + ":" + Squares.RIGHT);
+					break;
+				}
+			}
+		}
 	}
 	
-	protected boolean closable(int i, int j){
-        return lines(i,j)==3;
-    }
-    
-    protected boolean closed(int i, int j){
-        return lines(i,j)==4;
-    }
-    
-    protected void check(int i, int j){ 
-    	if(closed(i,j)) {
-    		cuenta++;
-    		recheck = false;
-    	}else{
-	    	if(closable(i,j)){
-	        if( (values[i][j] & Board.LEFT)==0 ){
-	            values[i][j] |= Board.LEFT;
-	            values[i][j-1] |= Board.RIGHT;
-	            cuenta++;
-	            check(i,j-1);
-	        }
-	
-	        if( (values[i][j] & Board.TOP)==0 ){
-	            values[i][j] |= Board.TOP;
-	            values[i-1][j] |= Board.BOTTOM;
-	            cuenta++;
-	            check(i-1,j);
-	        }
-	        
-	        if( (values[i][j] & Board.RIGHT)==0 ){
-	            values[i][j] |= Board.RIGHT;
-	            values[i][j+1] |= Board.LEFT;
-	            cuenta++;
-	            check(i,j+1);
-	        }
-	
-	        if( (values[i][j] & Board.BOTTOM)==0 ){
-	            values[i][j] |= Board.BOTTOM;
-	            values[i+1][j] |= Board.TOP;
-	            cuenta++;
-	            check(i+1,j);
-	        }
-    	}
-      }
+	protected String minMax (String nodo, boolean isMax, int profundidad, int[] puntos) {
+		System.out.println("profundidad: " + profundidad);
+		if (profundidad==0) {
+			//System.out.println(nodo + ":" + (puntos[0]-puntos[1]));
+			//System.out.println("p0 "+puntos[0]+ " p1 " +puntos[1]);
+			return nodo + ":" + (puntos[0]-puntos[1]);
+		}
+		//Hacer jugada y sacar los hijos
+		int[] punt=jugarMinMax(nodo,puntos,isMax);
+		
+		sacarJugadas(julio--);
+		//
+		
+		ArrayList<String> aux = (ArrayList<String>) moves.clone();
+		moves.clear();
+		if (isMax) {
+			String resultado = null;
+			int maxEva = Integer.MIN_VALUE;
+			for (String jugada:aux) {
+				historial.add(jugada);
+				resultado = minMax(jugada,false,profundidad-1,punt);
+				historial.remove(historial.size()-1);
+				arreglarMatriz();
+				julio++;
+			}
+			//System.out.println(Arrays.toString(resultado.split(":")));
+			return Integer.parseInt(resultado.split(":")[3]) > maxEva ? resultado : "";
+		} else {
+			String resultado = null;
+			int minEva = Integer.MAX_VALUE;
+			for (String jugada:aux) {
+				historial.add(jugada);
+				resultado = minMax(jugada,true,profundidad-1,punt);
+				historial.remove(historial.size()-1);
+				arreglarMatriz();
+				julio++;
+			}
+			//System.out.println(Arrays.toString(resultado.split(":")));
+			return Integer.parseInt(resultado.split(":")[3]) < minEva ? resultado : "";
+		}
 	}
-    
-    protected void detenerse (int a) {
-    	stop:
-    	for (int i = size-1; i >= 0;i--) {
-    		for (int j = size-1; j>=0; j--) {
-    			cuenta++;
-    			
-    			if (cuenta==a) {
-    				lastPosition[0]=i;
-    				lastPosition[1]=j;
-    				break stop;
-    			}
-    		}
-    	}
-    }
-    
+	
+	protected void arreglarMatriz() {
+		values = Arrays.stream(board).map(int[]::clone).toArray(int[][]::new);
+		for (String ss : historial) {
+			String[] s = ss.split(":");
+			int i = Integer.parseInt(s[0]);
+			int j = Integer.parseInt(s[1]);
+			if (s[2].equals(Squares.RIGHT)) {
+				values[i][j] |= Board.RIGHT;
+				values[i][j + 1] |= Board.LEFT;
+				check(i, j);
+				if (recheck)
+					check(i, j + 1);
+			}
+			if (s[2].equals(Squares.BOTTOM)) {
+				values[i][j] |= Board.BOTTOM;
+				values[i + 1][j] |= Board.TOP;
+				check(i, j);
+				if (recheck)
+					check(i + 1, j);
+			}
+			if (s[2].equals(Squares.LEFT)) {
+				values[i][j] |= Board.LEFT;
+				values[i][j - 1] |= Board.RIGHT;
+				check(i, j);
+				if (recheck)
+					check(i, j - 1);
+			}
+			if (s[2].equals(Squares.TOP)) {
+				values[i][j] |= Board.TOP;
+				values[i - 1][j] |= Board.BOTTOM;
+				check(i, j);
+				if (recheck)
+					check(i - 1, j);
+			}
+			cuenta=0;
+			recheck=true;
+		}
+	}
+	
+	protected int[] jugarMinMax (String jugada, int[] puntos, boolean isMax) {
+		String[] s = jugada.split(":");
+		int i = Integer.parseInt(s[0]);
+		int j = Integer.parseInt(s[1]);
+		int[] ar = new int[2];
+		if (s[2].equals(Squares.RIGHT)) {
+			values[i][j] |= Board.RIGHT;
+			values[i][j + 1] |= Board.LEFT;
+			check(i, j);
+			if (recheck)
+				check(i, j + 1);
+		}
+		if (s[2].equals(Squares.BOTTOM)) {
+			values[i][j] |= Board.BOTTOM;
+			values[i + 1][j] |= Board.TOP;
+			check(i, j);
+			if (recheck)
+				check(i + 1, j);
+		}
+		if (s[2].equals(Squares.LEFT)) {
+			values[i][j] |= Board.LEFT;
+			values[i][j - 1] |= Board.RIGHT;
+			check(i, j);
+			if (recheck)
+				check(i, j - 1);
+		}
+		if (s[2].equals(Squares.TOP)) {
+			values[i][j] |= Board.TOP;
+			values[i - 1][j] |= Board.BOTTOM;
+			check(i, j);
+			if (recheck)
+				check(i - 1, j);
+		}
+		if (isMax) {
+			ar[1] = puntos[1] + cuenta;
+		}else {
+			ar[0] = puntos[0] + cuenta;
+		}
+		cuenta=0;
+		recheck=true;
+		return ar;
+	}
+
 }
